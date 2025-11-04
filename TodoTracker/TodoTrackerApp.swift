4b7996d0f9366ee84717915c5103def96358e2b6
DS4506 <davidsmith4506@gmail.com>
2025-11-03 20:51:36 -0600

    Initial Commit

---
+++ TodoTracker/TodoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTrackerApp.swift
+//  TodoTracker
+//
+//  Created by Willie Earl on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
